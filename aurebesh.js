aa3bd75d36cbc9e1b955235f43ae8e2feb81b049
Martin Kleppe <kleppe@ubilabs.net>
2016-07-17 23:45:08 +0200

    update styles

--- aurebesh.js
+++ aurebesh.js
@@ -73,7 +73,7 @@ Object.keys(alphabets).forEach(name => {
   var alphabet = alphabets[name],
     translation = aurebesh(alphabet);
 
-  output.innerHTML += `<article><h3>${name.toUpperCase()}</h3><div class="alphabet">// ${alphabet}</div><div>${translation}</div></article>`;
+  output.innerHTML += `<article><h3>${name}</h3><div class="alphabet">// ${alphabet}</div><div>${translation}</div></article>`;
 });
 
 var input = document.getElementById('your-input'),
