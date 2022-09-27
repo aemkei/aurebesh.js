bd338f7e29dcf0e2b96c51df138c72b152d71b5f
Martin Kleppe <kleppe@ubilabs.net>
2022-09-27 21:45:23 +0200

    move boostrap loader to own file

---
+++ 2/load-bootstrap.js
@@ -0,0 +1,42 @@
+async function loadBootstrapCode() {
+  const fs = require('fs');
+  const path = require('path');
+  const { minify } = require("terser");
+
+  const file = path.resolve(__dirname, 'core.js')
+  const code = fs.readFileSync(file, 'utf8');
+
+  const options = {
+    module: true,
+    compress: false,
+    mangle: false,
+    rename: {},
+  };
+
+  // tha character map to use for the variables
+  const alphabet = [..."ABCDEFGHIJKLMNOPQRSTUVWX"];
+
+  // holds the mapping of single characters to original names
+  const mapping = {};
+
+  // load and minify the bootstrap code from core.js
+  let bootstrap = (await minify(code, options)).code;
+
+  // remove all 'const' overhead from the orignal code
+  bootstrap = bootstrap.replace('const ', '');
+
+  // replace names in original code with alphabet
+  bootstrap = bootstrap.replace(/[A-Z_0-9]+/g, function (name) {
+    // get the next unused character
+    if (!mapping[name]) {
+      mapping[name] = alphabet.shift()
+    }
+    return mapping[name];
+  });
+
+  console.log({bootstrap, mapping});
+
+  return {bootstrap, mapping};
+}
+
+module.exports = loadBootstrapCode;
